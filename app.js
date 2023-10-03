581a6013534caf9e6e7886346cc45f1cd05cc2a0
Krixxx <32840063+Krixxx@users.noreply.github.com>
2023-10-03 20:26:26 +0300

    Ex 4.2 add morgan for logging and mongoose connection logs

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 const blogsRouter = require('./controllers/blog')
 const middleware = require('./utils/middleware')
-require('dotenv').config()
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const mongoUrl = process.env.MONGO_URI
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 app.use('/api/blogs', blogsRouter)
 
