87920b8b292aa991ea75b110a3d134617c23834a
Krixxx <32840063+Krixxx@users.noreply.github.com>
2023-10-08 15:56:57 +0300

    Remove logs when running tests

--- app.js
+++ app.js
@@ -25,9 +25,13 @@ app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
+if (process.env.NODE_ENV !== 'test') {
+  app.use(
+    morgan(
+      ':method :url :status :res[content-length] - :response-time ms :body'
+    )
+  )
+}
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
