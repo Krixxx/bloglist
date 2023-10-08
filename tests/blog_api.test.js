229b6f4580df204d5ceb9e2874634a53f0227561
Krixxx <32840063+Krixxx@users.noreply.github.com>
2023-10-08 12:52:46 +0300

    Ex 4.12 Blog list tests, step 5

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -69,6 +69,34 @@ test('blog without likes property is added  with default value of 0', async () =
   expect(response.body.likes).toBe(0)
 })
 
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+
+  const blogsAfterSaving = await helper.blogsInDb()
+
+  expect(blogsAfterSaving).toHaveLength(helper.initialBlogs.length)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    likes: 10,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+
+  const blogsAfterSaving = await helper.blogsInDb()
+
+  expect(blogsAfterSaving).toHaveLength(helper.initialBlogs.length)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
